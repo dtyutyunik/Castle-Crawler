9cd182b79ba9d4c8b1b76a6dabd867095779efd3
Dima <dmitriy.tyutyunik@gmail.com>
2018-10-26 15:49:09 -0400

    mvp met

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const game = document.querySelector(".gameInner");
-const skeleton = document.querySelector(".skeleton").style;
+// const game = document.querySelector(".gameInner");
 const character = document.querySelector(".character");
 const gameDesign = document.querySelector('.gameInner');
 let gameWidth=gameDesign.style.width;
 let gameHeight=gameDesign.style.height;
 
-let backgroundSong=new Audio("background.mp3");
-let deadSong=new Audio("dead.mp3");
-backgroundSong.play();
-//Item creations via variable setting
-const hero = {x: 0,y: 0, life: 3};
 
-// let stairCase = [{x: parseInt(gameWidth/100)-1,y: parseInt(gameHeight/100)-1}];
-let stairCase = [{x: 9  ,y: 7}];
 
+let clock; //clock is timekeeper interval
+let gameover=true; //gameover is a variable that is used to stop eventlistener from working
+let backgroundSong=new Audio("background.mp3");   //background song
+let deadSong=new Audio("dead.mp3"); //song to be played when hurt
+
+//coordinates of elements used in game
+let hero = {x: 0,y: 0, life:3};
+let stairCase = [{x: 9 ,y: 7}];
 let walls = [
   {x: 1,y: 1},
   {x: 1,y: 2},
@@ -42,7 +42,6 @@ let walls = [
   {x: 9,y: 4},
   {x: 9,y: 1}
 ];
-
 let skeletons = [
   {x: 2, y: 3},
   {x: 2, y: 1},
@@ -51,54 +50,34 @@ let skeletons = [
   {x: 2, y: 6},
   {x: 8, y: 6}
 ];
+// let heart=[{x:0,y:5}];
 
-
-let heart=[{x:0,y:5}];
-
-
-// function placeObjects(name,item, srcImage,size){
-  // n.setAttribute("class", `${name}${i}`); //sets the class attribute with the name
-  // placeObjects("walls", walls, "wall.png",100);
-
-
-
-
+//creates the board, can render it on 3 paramters if need be
 function createBoard(width, height, color){
-
-  //max size is 800
+  //max size is 800 due to size of screen
   if(height>=800){
     gameHeight=800;
   }
   else{
     gameHeight= height;
   }
-
   gameWidth= width;
   //these get reasigned to the dimesnions of the game
   gameDesign.style.width = gameWidth + "px";
   gameDesign.style.height = gameHeight + "px";
   gameDesign.style.background = color;
-
 }
-
-
-
 
 //send through name of array, object of the array in question and url image, size is the px image of the item
 function placeObjects(name,item, srcImage,size){
-
-// console.log(srcImage);
 let newItem=document.createElement('div');
 
-  //this gets me the x and y cordinates of the items
   for(let i=0;i<item.length;i++){
-
-    //this creates the elemnt of div
         let n = document.createElement(`div`);
         n.setAttribute("class", `${name}${i}`); //sets the class attribute with the name
         n.setAttribute("style",
         `background-image: url(${srcImage});
-         max-width: ${size}px;
+        max-width: ${size}px;
          background-size:contain;
          background-repeat:no-repeat;
          max-height:${size}px;
@@ -109,40 +88,57 @@ let newItem=document.createElement('div');
          top:${item[i].y*100+"px"}`
        );
 
-    game.append(n); //adds each element to the parent
+    gameDesign.append(n); //adds each element to the parent
   }
 }
 
+//loads level 1, places the objects and makes them move
 function level1(){
   createBoard(1000,1000,"black");
+  placeObjects("hero", hero, "hero.gif",100);
   placeObjects("walls", walls, "wall.png",100);
   placeObjects("skeletons",skeletons,"skeleton.gif",100);
   placeObjects("stairCase",stairCase,"stair.png",100);
-  placeObjects("heart",heart,"hearts-full.png",50);
   skeleMove(".skeletons0","left","right",420);
   skelMoveShort(".skeletons1","up","down",500);
   skeleMove(".skeletons2","left","right",400);
   skeleMove(".skeletons3","up","down",380);
   skelMoveShort(".skeletons4","up","down",500);
   skelMoveShort(".skeletons5","left","right",380);
+  // backgroundSong.play();
+  showPoints();
 
+  setInterval(function(){
+    touchSomething(skeletons,"skeletons");
+    touchSomething(stairCase,"stairCase");
+  },200);
+
+  clock=setInterval(showTime,1000); //activates the countdown clock
 }
 
+let dragon = [
+  {x: 5, y: 7}];
+
 function finalLevel(){
-  // createBoard(1000,1000,"black");
-  // placeObjects("walls", walls, "wall.png",100);
+
   createBoard(1000,1000,"black");
   removeStuff("walls",walls);
   removeStuff("skeletons",skeletons);
-  removeStuff("heart",heart);
   removeStuff("stairCase",stairCase);
-  skeletons = [
-    {x: 5, y: 7}];
-placeObjects("skeletons",skeletons,"skeleton.gif",100);
-    // skelMoveShort(".skeletons0","left","right",300);
+  placeObjects("dragon",dragon,"drag.gif",100);
+  skelMoveShort(".dragon0","left","right",300);
 
+
+
+  // clearInterval(function(){
+    // touchSomething(skeletons,"skeletons");
+    // touchSomething(stairCase,"stairCase");
+  // },350);
+
+  setInterval(function(){touchSomething(dragon,"dragon")},150);
   placeObjects("princess",princess,"prin2.gif",100);
-walls=[
+
+  walls=[
   {x: 3,y: 7},
   {x: 4,y: 6},
   {x: 4,y: 5},
@@ -165,15 +161,13 @@ walls=[
   placeObjects("walls", walls, "wall.png",100);
 }
 
-level1();
-// finalLevel();
+// setInterval(function(){
+//   touchSomething(skeletons,"skeletons");
+//   touchSomething(stairCase,"stairCase");
+// },200);
 
-// placeObjects("walls", walls, "wall.png",100);
-// placeObjects("skeletons",skeletons,"skeleton.gif",100);
-// placeObjects("stairCase",stairCase,"stair.png",100);
-// placeObjects("princess",princess,"prin2.gif",100);
-// placeObjects("heart",heart,"hearts-full.png",50);
 
+level1(); //Calls Level1 of the game
 
 //sets parameters of arena
 const gameArena = (x,y) =>{
@@ -192,22 +186,24 @@ const canMoveTo = (x , y) => {
   return true;
 }
 
-
 const moveHeroTo = (x, y) => {
-  // Multiply the coordinates by 100 because each grid square
-  // is 100x100 pixels in size.
+  // Multiply the coordinates by 100 because each grid square is 100x100 pixels in size.
   character.style.top = (y * 100).toString() + 'px';
   character.style.left = (x * 100).toString() + 'px';
 };
 
 
 
-//eventlistener to the board
+
 document.addEventListener("keyup", keys=> {
   //preventDefault stops the keys from doing their default property such as moving screen
-    if([37,38,39,40,65].includes()){
+if(gameover===true){
+
+    if([37,38,39,40,65].includes(keys.keyCode)){
+      console.log("key pressed");
       keys.preventDefault();
     }
+
     //keys is the value that is passed through upon releasing key, and keyCode is the special terminology that is needed to make it work
     //which is used for browser support incase opened in firefox, otherwise we use keycode
     switch (keys.keyCode || keys.which) {
@@ -215,9 +211,13 @@ document.addEventListener("keyup", keys=> {
     case 38: moveUp(hero); break;
     case 39: moveRight(hero); break;
     case 40: moveDown(hero); break;
-    case 65: touchSomething(princess,"princess");break;
+    case 65: touchSomething(princess,"princess"); console.log("princess"); break;
   }
+}
 });
+
+
+
 
 
 function wallTouch3(direction) {
@@ -242,21 +242,17 @@ function wallTouch3(direction) {
 
 //hero is the coordinates of the character
 const moveUp = (item) => {
-
     if(canMoveTo(item.x,item.y-1)&&wallTouch3('up') === false){
       item.y -= 1;
       moveHeroTo(item.x, item.y);
     }
-
 };
 
 const moveDown = (item) => {
-
   if (canMoveTo(item.x, item.y+1)&&wallTouch3('down') === false){
     item.y += 1;
     moveHeroTo(item.x, item.y);
   }
-
 };
 
 const moveLeft = (item) => {
@@ -265,6 +261,7 @@ const moveLeft = (item) => {
     item.x-=1;
     moveHeroTo(item.x,item.y);
   }
+  character.style.transform="scaleX(-1)"; //this changes the direction the character appears to look
 
 };
 
@@ -272,6 +269,7 @@ const moveRight = (item) => {
   if(canMoveTo(item.x+1,item.y)&&wallTouch3('right') === false){
     item.x+=1;
     moveHeroTo(item.x,item.y);
+    character.style.transform="scaleX(1)";
   }
 
 
@@ -285,7 +283,6 @@ function touchSomething(item,itemString){
   let charRight=parseInt(charLeft)+100;
   let charTop=parseInt(character.style.top);
   let charBottom=charTop+100;
-
 
   for(let i=0;i<item.length;i++){
 
@@ -302,8 +299,13 @@ function touchSomething(item,itemString){
               deadSong.play();
               backgroundSong.play();
           }
+
+          if(itemString==="dragon"){
+            loseLife();
+          }
           else if(itemString==="princess"){
               addToBoard("alive");
+              gameover=false; //removes event listener and freezes game in place
           }
           else if(itemString==="stairCase")
             {
@@ -311,16 +313,30 @@ function touchSomething(item,itemString){
             }
       }
   }
+
 }
 
+function moveDrag(element,direction){
+  //this  needs to be put in as an error determent when removing skelletons
+  if(dragon.length!==0){
+    let drag=document.querySelector(element);
 
-setInterval(function(){
-  touchSomething(skeletons,"skeletons");
-  touchSomething(stairCase,"stairCase");
+    posLeft = parseInt(drag.style.left);
+    posTop = parseInt(drag.style.top);
 
-},300);
+    switch(direction){
+      case "down": posTop += 100; break;
+      case "up": posTop -= 100; break;
+      case "right": posLeft += 100; break;
+      case "left": posLeft -= 100; break;
+    }
+
+    drag.style.left = posLeft + 'px';
+    drag.style.top = posTop + 'px';
+  }
 
 
+}
 
 function moveSkel(element,direction){
 
@@ -341,30 +357,45 @@ if(skeletons.length!==0){
   skel.style.left = posLeft + 'px';
   skel.style.top = posTop + 'px';
 }
-// else{
-  // console.log("alal");
-// }
+
 }
 
 function skelMoveShort(enemy,direction1,direction2,time){
-  if(skeletons!==[]){
+  if(skeletons.length!==0){
     setTimeout(function(){moveSkel(enemy,direction1)},time);
     setTimeout(function(){moveSkel(enemy,direction2)},time*2);
     setTimeout(function(){moveSkel(enemy,direction2)},time*3);
     setTimeout(function(){moveSkel(enemy,direction1)},time*4);
+
+    setInterval(function()
+    {
+      setTimeout(function(){moveSkel(enemy,direction1)},time);
+      setTimeout(function(){moveSkel(enemy,direction2)},time*2);
+      setTimeout(function(){moveSkel(enemy,direction2)},time*3);
+      setTimeout(function(){moveSkel(enemy,direction1)},time*4);
+    },time*4);
   }
-  setInterval(function()
-  {
-    setTimeout(function(){moveSkel(enemy,direction1)},time);
-    setTimeout(function(){moveSkel(enemy,direction2)},time*2);
-    setTimeout(function(){moveSkel(enemy,direction2)},time*3);
-    setTimeout(function(){moveSkel(enemy,direction1)},time*4);
-  },time*4);
+  else if(dragon.length===1){
+    setTimeout(function(){moveDrag(enemy,direction1)},time);
+    setTimeout(function(){moveDrag(enemy,direction2)},time*2);
+    setTimeout(function(){moveDrag(enemy,direction2)},time*3);
+    setTimeout(function(){moveDrag(enemy,direction1)},time*4);
+
+    setInterval(function()
+    {
+      setTimeout(function(){moveDrag(enemy,direction1)},time);
+      setTimeout(function(){moveDrag(enemy,direction2)},time*2);
+      setTimeout(function(){moveDrag(enemy,direction2)},time*3);
+      setTimeout(function(){moveDrag(enemy,direction1)},time*4);
+    },time*4);
+  }
+
 }
 
 function skeleMove(enemy,direction1,direction2,time) {
 console.log(skeletons);
-  if(skeletons!==[]){
+console.log(dragon);
+  if(skeletons.length!=0){
   //it moves the skeletion from beginning for 2 secs
   setTimeout(function(){moveSkel(enemy,direction1)},time);
   setTimeout(function(){moveSkel(enemy,direction1)},time*2);
@@ -390,20 +421,11 @@ console.log(skeletons);
 }
 
 }
-//we call the function so it activates right away
-// skeleMove(".skeletons0","left","right");
-// // skeleMove(".skeletons1","up","down");
-// skeleMove(".skeletons2","left","right");
-// skeleMove(".skeletons3","up","down");
 
-
-
-
-//add life
+//add life--not implemented
 function addLife(){
   if(hero.life<3){
     hero.life++;
-    console.log("life+1");
     displayHearts();
   }
   else{console.log("you are at max life");}
@@ -414,29 +436,26 @@ function addLife(){
 function loseLife(){
   if(hero.life>=1){
     hero.life--;
-
-    console.log("life-1");
     displayHearts();
   }
-  else{console.log("you are dead");}
 }
 
 function createHearts(){
 
 let heartPlace=document.querySelector(".heartContainer");
-for(let i=0;i<3;i++){
-  let n=document.createElement('div');
-  n.setAttribute("class", `hearts${i}`);
-  n.setAttribute("style",
-  `background-image: url("hearts-full.png");
-  background-size:contain;
-  flex:1;
-  background-repeat:no-repeat;
-  width: 100px;
-  height:100px`
-  );
-  heartPlace.append(n);
 
+for(let i=0;i<3;i++){
+    let n=document.createElement('div');
+      n.setAttribute("class", `hearts${i}`);
+      n.setAttribute("style",
+        `background-image: url("hearts-full.png");
+        background-size:contain;
+        flex:1;
+        background-repeat:no-repeat;
+        width: 100px;
+        height:100px`
+      );
+    heartPlace.append(n);
   };
 }
 
@@ -466,8 +485,9 @@ function displayHearts(){
     heart0="url('hearts-empty.png')";
     heart1="url('hearts-empty.png')";
     heart2="url('hearts-empty.png')";
-
     addToBoard("dead");
+    gameover=false;
+
   }
   document.querySelector('.hearts0').style.backgroundImage=heart0;
   document.querySelector('.hearts1').style.backgroundImage=heart1;
@@ -477,30 +497,31 @@ function displayHearts(){
 
 
 
-let i=15;
+let timer=500;
 
 function showTime(){
 
-  let over60=Math.floor(i/60);
-  let rem60=Math.floor(i%60);
+  let over60=Math.floor(timer/60);
+  let rem60=Math.floor(timer%60);
+  let display=document.querySelector(".timeAlive").innerText;
 
-let display=document.querySelector(".timeAlive").innerText;
-
-  if(i<0){
+  if(timer<0){
     display="Game Over";
     document.querySelector(".timeAlive").style.color="red";
+    document.querySelector(".timeAlive").style.fontSize="40px";
     stopTheClock();
     addToBoard("dead");
+    gameover=false;
 
   }
 
-  else if(i<10){
-    display=` 0:0${i}`;
+  else if(timer<10){
+    display=` 0:0${timer}`;
   }
-  else if(i<60){
-    display=`0:${i}`;
+  else if(timer<60){
+    display=`0:${timer}`;
   }
-  else if(i>=60){
+  else if(timer>=60){
     if(rem60<10){
       display=`${over60}:0${rem60}`;
     }
@@ -509,12 +530,10 @@ let display=document.querySelector(".timeAlive").innerText;
     }
   }
 
-  i--;
+  timer--;
   document.querySelector(".timeAlive").innerText=`${display}`;
 
 }
-
-let clock =  setInterval(showTime,1000);
 
 function stopTheClock(){
   clearInterval(clock);
@@ -538,7 +557,7 @@ var scoreKeep= [
 
 let printScore=JSON.parse(localStorage.getItem('scores'));
 
-showPoints();
+
 
 function addToBoard(howIdo){
   //if empty intialize with hardcoded scoreKeep
@@ -547,12 +566,16 @@ function addToBoard(howIdo){
     updateScore=scoreKeep;
   }
 
-  let whoMe=prompt("what is your name?");
-  updateScore.push({name: whoMe, time: i, status:howIdo});
+  let whoMe=prompt("What is your name for the record books?");
+  updateScore.push({name: whoMe, time: timer, status:howIdo});
   console.log(scoreKeep);
   localStorage.setItem('scores', JSON.stringify(updateScore));
   // localStorage.setItem('scores', JSON.stringify(scoreKeep));
+
   showPoints();
+  alert("The game will reload in 3 seconds");
+setTimeout(reloadPage,3000);
+
 }
 
 
@@ -560,12 +583,9 @@ function addToBoard(howIdo){
 
 function showPoints(){
 let updateScore=JSON.parse(localStorage.getItem('scores'));
-// printScore=JSON.parse(localStorage.getItem('scores'));
 let infoShame="";
 let infoFame="";
 
-
-  // for(let i=0;i<updateScore.length;i++){
 for(let i=0;i<updateScore.length;i++){
 
     if(updateScore[i].status==="dead"){
@@ -577,13 +597,14 @@ for(let i=0;i<updateScore.length;i++){
   }
     document.querySelector('.fame').innerHTML= infoFame;
     document.querySelector('.shame').innerHTML= infoShame;
+    // reloadPage();
 }
 
 
 
 function removeStuff(name,item){
   //makes the array 0
-console.log(item.length-1);
+// console.log(item.length-1);
   for(let i=item.length-1;i>=0;i--){
     document.querySelector(`.${name}${i}`).remove();
   }
@@ -596,4 +617,18 @@ if(item===skeletons){
 skeletons = [];
 }
 
+if(item===princess){
+  princess=[];
+}
+
+if(item===stairCase){
+  stairCase=[];
+}
+
+}
+
+
+function reloadPage(){
+
+  window.location.reload();
 }
